680a6c4734d6461cb4bf7ffef9c0b78160718716
Govind Lohar <govindloharpersonal@ibms-MacBook-Pro-85.local>
2026-01-06 10:19:17 +0530

    feat: add database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HealthModule } from './health/health.module';
-import { UsersModule } from './users/users.module';
-import { UserAddressesModule } from './user-addresses/user-addresses.module';
-import { CarsModule } from './cars/cars.module';
-import { UsersModule } from './users/users.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [HealthModule, UsersModule, CarsModule, UserAddressesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE,
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT!,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'], // instead of we written autoLoadEntities:true
+      autoLoadEntities: true, // take entity from typeOrmModule.forFeature([]) and load here
+      synchronize: false,
+      // if it is true then typeorm will talk to db schema if tables not present will create , update , add column ,drop ect
+    } as TypeOrmModuleOptions),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
