95bbf10ededf6ca96f46534d341fb710c078ac80
Govind Lohar <govindloharpersonal@ibms-MacBook-Pro-85.local>
2026-01-07 10:36:27 +0530

    feat : add booking-entity

---
+++ src/bookings/entities/booking-history.ts
@@ -0,0 +1,68 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Car } from 'src/cars/entities/car.entity';
+import { BookingStatus } from '../enums/booking.enum';
+@Entity('booking_histories')
+export class BookingHistory {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => Car, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'car_id' })
+  car: Car;
+
+  @Column({ type: 'float', nullable: false })
+  price: number;
+
+  @Column({ type: 'enum', enum: BookingStatus })
+  status: BookingStatus;
+
+  @Column({ name: 'start_date', nullable: false, type: 'timestamptz' })
+  startDate: Date;
+
+  @Column({ name: 'end_date', nullable: false, type: 'timestamptz' })
+  endDate: Date;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    name: 'created_at',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMPZ',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    name: 'updated_at',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMPZ',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    type: 'timestamptz',
+    name: 'deleted_at',
+    nullable: true,
+    default: null,
+  })
+  deletedAt: Date;
+}
