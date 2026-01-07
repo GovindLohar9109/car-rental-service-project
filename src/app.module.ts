7426b67bc1344c5cb121e448fd451e42f7af9fa0
Govind Lohar <govindloharpersonal@ibms-MacBook-Pro-85.local>
2026-01-07 18:08:17 +0530

    chore : update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import dotenv from 'dotenv';
 import { HealthModule } from './health/health.module';
 import { dataSourceOptions } from './db/data-source';
-import { StudentsModule } from './students/students.module';
 
 dotenv.config();
 
 @Module({
-  imports: [HealthModule, TypeOrmModule.forRoot(dataSourceOptions), StudentsModule],
+  imports: [
+    HealthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: false,
+      autoLoadEntities: true, // take entity from typeOrmModule.forFeature([]) and load here
+      migrations: ['src/migrations/*.ts'],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
