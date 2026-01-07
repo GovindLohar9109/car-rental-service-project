f1456c94ff9dc662fcac7e039cc1a2959e996a8a
Govind Lohar <govindloharpersonal@ibms-MacBook-Pro-85.local>
2026-01-07 10:44:27 +0530

    feat : add state-entity

---
+++ src/states/entities/state.entity.ts
@@ -0,0 +1,50 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Country } from 'src/countries/entities/country.entities';
+@Entity('states')
+export class State {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 40, nullable: false })
+  name: string;
+
+  @ManyToOne(() => Country, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'country_id' })
+  country: Country;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    name: 'created_at',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMPZ',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    name: 'updated_at',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMPZ',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    type: 'timestamptz',
+    name: 'deleted_at',
+    nullable: true,
+    default: null,
+  })
+  deletedAt: Date;
+}
