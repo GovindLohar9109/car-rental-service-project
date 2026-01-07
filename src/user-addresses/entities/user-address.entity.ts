b59d9d10f846f54b6a75a99f5447d33ccdfa7be2
Govind Lohar <govindloharpersonal@ibms-MacBook-Pro-85.local>
2026-01-07 10:47:21 +0530

    chore : update user-address.entity.ts

--- src/user-addresses/entities/user-address.entity.ts
+++ src/user-addresses/entities/user-address.entity.ts
@@ -1,1 +1,69 @@
-export class UserAddress {}
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+import { Country } from 'src/countries/entities/country.entities';
+import { State } from 'src/states/entities/state.entity';
+import { City } from 'src/cities/entities/city.entity';
+@Entity('addresses')
+export class Address {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false, name: 'address_line', length: 100 })
+  addressLine: string;
+
+  @ManyToOne(() => Country, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'country_id' })
+  country: Country;
+
+  @ManyToOne(() => State, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'state_id' })
+  state: State;
+
+  @ManyToOne(() => City, {
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'city_id' })
+  city: City;
+
+  @Column({ length: 15, name: 'zip' })
+  zip: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    name: 'created_at',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMPZ',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    name: 'updated_at',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMPZ',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    type: 'timestamptz',
+    name: 'deleted_at',
+    nullable: true,
+    default: null,
+  })
+  deletedAt: Date;
+}
