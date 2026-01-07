07e8a23c15ba581cfe5484dccf21909abba5d6d2
Govind Lohar <govindloharpersonal@ibms-MacBook-Pro-85.local>
2026-01-07 10:41:55 +0530

    feat : add country-entity

---
+++ src/countries/entities/country.entities.ts
@@ -0,0 +1,40 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+@Entity('countries')
+export class Country {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 40, nullable: false })
+  name: string;
+
+  @CreateDateColumn({
+    type: 'timestamptz',
+    name: 'created_at',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMPZ',
+  })
+  createdAt: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamptz',
+    name: 'updated_at',
+    nullable: false,
+    default: () => 'CURRENT_TIMESTAMPZ',
+  })
+  updatedAt: Date;
+
+  @DeleteDateColumn({
+    type: 'timestamptz',
+    name: 'deleted_at',
+    nullable: true,
+    default: null,
+  })
+  deletedAt: Date;
+}
